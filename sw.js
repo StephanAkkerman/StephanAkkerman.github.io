81f789e0d9e8bb6802906dbceaef3cdd585eb5d8
Akkie100 <stephan.akkerman@gmail.com>
2021-09-13 20:37:21 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-86268292862d8cb3112a.js"
+    "url": "webpack-runtime-9d30f4c2573a46dcee5e.js"
   },
   {
     "url": "framework-aa35e29fbb74d6100667.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "7578d79a035905bf0eab2cbac00fdd80"
+    "revision": "dc961d1e83683d20310d0aa59325b4ce"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-bafda31d2d983398e3d8.js"
